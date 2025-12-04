5a51dabe6558c81a893441fb21245bd6ff77b28e
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-12-04 22:25:53 +0530

    Time: 16 ms (13.32%), Space: 51.7 MB (5.21%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -4,7 +4,7 @@ class Solution {
         return check(s, 0);
     }
 
-    private boolean check(String s, int i) {
+    public boolean check(String s, int i) {
         if (i >= s.length() / 2)
             return true;
         if (s.charAt(i) != s.charAt(s.length() - i - 1))
