5a0ad32ce69de943bca1954883f9d90aa3e1fc28
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-12-07 22:21:42 +0530

    Time: 2 ms (85.26%), Space: 47.9 MB (6.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,5 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
+    public void moveZeroes(int[] nums) { // fill non zeroes then fill zeros
         int i = 0;
         int n = nums.length;
         for (int j = 0; j < n; j++) {
@@ -14,7 +14,7 @@ class Solution {
         }
     }
 
-    public void moveZeroe(int[] nums) {
+    public void moveZeroe(int[] nums) { // swaping
         int i = 0;
         int n = nums.length;
         for (int j = 0; j < n; j++) {
