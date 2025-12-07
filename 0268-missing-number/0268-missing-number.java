4e9bb2aea353c260e17ff0f8cc38b0776ac1f511
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-12-07 22:39:00 +0530

    Time: 0 ms (100%), Space: 47.2 MB (43.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,49 @@
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumber(int[] nums) { // XOR
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans = ans ^ i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+
+    public int missingNumber1(int[] nums) { // sort -> edge case -> traverse
         Arrays.sort(nums);
         int n = nums.length;
-        if (nums[0] != 0) return 0;
-        if (nums[n - 1] != n) return n;
+        if (nums[0] != 0)
+            return 0;
+        if (nums[n - 1] != n)
+            return n;
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] != i) return i;
+            if (nums[i] != i)
+                return i;
+        }
+        return 0;
+    }
+
+    public int missingNumber2(int[] nums) { // running sum
+        int sum = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            sum += i - nums[i];
+        }
+        return sum;
+    }
+
+    public int missingNumber3(int[] nums) { // resposition in another array a/c to idx
+        int n = nums.length;
+        int arr[] = new int[n + 1];
+        Arrays.fill(arr, -1);
+        for (int i = 0; i < n; i++) {
+            arr[nums[i]] = nums[i];
+        }
+        for (int i = 0; i < n + 1; i++) {
+            if (arr[i] == -1)
+                return i;
         }
         return 0;
     }
 }
-
