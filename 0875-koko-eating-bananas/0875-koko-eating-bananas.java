dcc3601877da8226da3a6d09d245c8b556af1d3a
Mayank Maurya <hpmayankmaurya@gmail.com>
2025-12-30 21:19:22 +0530

    Time: 28 ms (13.14%), Space: 48.4 MB (13.21%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,50 +1,21 @@
 class Solution {
-    public int max(int[] arr) {
-        int max = 0;
-        for (int num : arr) {
-            max = Math.max(max, num);
-        }
-        return max;
-    }
-
-    public int calculateHrs(int[] arr, int h) {
-        int sum = 0;
-        for (int j = 0; j < arr.length; j++) {
-            sum += Math.ceil((double) arr[j] / h);
-        }
-        return sum;
-    }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int left = 1;
-        int right = max(piles);
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int hrs = calculateHrs(piles, mid);
-            if (hrs <= h)
-                right = mid;
-            else
-                left = mid + 1;
-        }
-        return left;
-    }
-
-    // brute force
-    public int bruteforce(int[] piles, int h) {
         int max = 0;
         int n = piles.length;
         for (int i = 0; i < n; i++) {
             max = Math.max(max, piles[i]);
         }
-
-        for (int i = 1; i < max; i++) {
+        int left = 1;
+        int right = max;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
             int sum = 0;
             for (int j = 0; j < n; j++) {
-                sum += Math.ceil((double) piles[j] / i);
+                sum += Math.ceil((double) piles[j] / mid);
             }
-            if (sum <= h)
-                return i;
+            if (sum <= h) right = mid;
+            else left = mid + 1;
         }
-        return max;
+        return left;
     }
 }
