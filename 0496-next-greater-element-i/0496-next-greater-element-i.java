d0d9b7ef273338b99e611296d681aafa5b647813
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-01-30 02:03:54 +0530

    Time: 3 ms (92.27%), Space: 44.7 MB (93.25%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,5 +1,28 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int ans[] = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            int j = 0;
+            while (nums2[j] != nums1[i])
+                j++;
+            ans[i] = -1;
+
+            for (j = j + 1; j < n; j++) {
+                if (nums1[i] < nums2[j]) {
+                    ans[i] = nums2[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+class Solution_HashMap {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Stack<Integer> st = new Stack<>();
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int num : nums2) {
