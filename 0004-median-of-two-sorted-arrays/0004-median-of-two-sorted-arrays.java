cdef67f5e3785812fb0f80c72603160daf6532b2
Mayank Maurya <hpmayankmaurya@gmail.com>
2026-02-04 03:30:59 +0530

    Time: 1 ms (99.45%), Space: 49.3 MB (12.7%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,26 +1,30 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int x=nums1.length;
-        int y=nums2.length;
+        int n = nums1.length;
+        int m = nums2.length;
+        int i = 0;
+        int j = 0;
+        int m1 = 0;
+        int m2 = 0;
 
-        int[] combo = new int[x+y];
-        int k=0;
-        for(int i=0; i<x; i++){
-            combo[k]=nums1[i];
-            k++;
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j])
+                    m1 = nums2[j++];
+                else
+                    m1 = nums1[i++];
+            } else if (i < n)
+                m1 = nums1[i++];
+            else
+                m1 = nums2[j++];
         }
-        for(int i=0; i<y; i++){
-            combo[k]=nums2[i];
-            k++;
-        }
-        Arrays.sort(combo);
-        int z=combo.length;
 
-        if (z%2==1){
-            return combo[z/2];
-        }
-        else {
-            return ((combo[z/2]+combo[z/2-1])/2.000);
+        if ((n + m) % 2 == 1) {
+            return (double) m1;
+        } else {
+            double ans = (double) m1 + (double) m2;
+            return ans / 2.0;
         }
     }
 }
